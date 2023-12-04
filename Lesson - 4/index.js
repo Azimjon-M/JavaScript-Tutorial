02224cc089c5c4126a1e610b9ee02bd26e885eb1
Azimjon-M <azimeliboev2000@gmail.com>
2023-12-04 11:13:22 +0500

    Type Convirsions

---
+++ Lesson - 4/index.js
@@ -0,0 +1,12 @@
+// Type Convirsions
+let a = 1;
+let b = "5";
+let c = a+b
+console.log(typeof c);  //String 1 + '5' = 15 
+let d = a+ +b
+console.log(d);  //String 1 + 5 = 6
+
+// NaN  Not a Number
+let q = "23a";
+let y = 123;
+console.log(+q); //NaN +q ++q
