286c852a4b5e5ce750c8fbb0b714c6983800e271
Azimjon-M <azimeliboev2000@gmail.com>
2023-12-04 13:08:55 +0500

    If Else

---
+++ Lesson - 8/index.js
@@ -0,0 +1,20 @@
+// If Else
+// if (Conditional Operator) {Code}
+// else if (Conditional Operator) {Code}
+// else {Code}
+
+if (true) {
+    // Code
+}
+
+if (false) {
+    // Don't work
+} else if (true) {
+    // Code 
+}
+
+if (false) {
+    // Don't work
+} else {
+    // Code
+}
