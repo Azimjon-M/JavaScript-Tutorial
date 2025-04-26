ff020535b7cf6d1567d08aa500a8a4b9a602b8d5
Azimjon-M <azimeliboev2000@gmail.com>
2025-04-26 11:17:21 +0500

    Nulish operation

---
+++ Lesson - 10/index.js
@@ -0,0 +1,10 @@
+// ?? = Nullish. null va undefined qiymatlarni aniqlash
+let a = null;
+let b = undefined;
+let c = 123;
+
+a ?? console.log("a = null");
+b ?? console.log("b = undefined");
+c ?? console.log("ishlamaydi !");
+
+
